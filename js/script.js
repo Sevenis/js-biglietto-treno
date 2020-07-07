84ff54cca1730577241a15a077ce781be5505380
Pietro Sette <pietrosette@gmail.com>
2020-07-07 16:27:05 +0200

    Ticket Treno Done

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
 var km = parseInt(prompt('Quanti km vuoi percorrere?'));
 var eta = parseInt(prompt('Quanti anni hai?'));
 var costoBiglietto = (km * 0.21);
-console.log(costoBiglietto);
+
 if (eta < 18) {
     costoBiglietto *= 0.8;
-    console.log(costoBiglietto);
 } else if (eta > 65) {
     costoBiglietto *= 0.6;
-    console.log(costoBiglietto);
 }
 
-document.getElementById('prezzo-finale').innerHTML = 'Il prezzo del tuo biglietto è di € ' + costoBiglietto + '!';
+var costoBigliettoArrotondato = (parseInt(costoBiglietto*100))/100;
+document.getElementById('prezzo-finale').innerHTML = 'Il prezzo del tuo biglietto è di € ' + costoBigliettoArrotondato + '!';
