c5c23905a1d2d60e62bc368fce08569da1e07fa9
Pietro Sette <pietrosette@gmail.com>
2020-07-08 10:42:38 +0200

    Checked

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
+// dati di input
 var km = parseInt(prompt('Quanti km vuoi percorrere?'));
 var eta = parseInt(prompt('Quanti anni hai?'));
-var costoBiglietto = (km * 0.21);
+var prezzo = 0.21;
 
+// operazione
+var costoBiglietto = km * prezzo;
+
+// condizione
 if (eta < 18) {
     costoBiglietto *= 0.8;
 } else if (eta > 65) {
     costoBiglietto *= 0.6;
 }
 
-var costoBigliettoArrotondato = (parseInt(costoBiglietto*100))/100;
-document.getElementById('prezzo-finale').innerHTML = 'Il prezzo del tuo biglietto è di € ' + costoBigliettoArrotondato + '!';
+costoBiglietto = (parseInt(costoBiglietto*100))/100;
+document.getElementById('prezzo-finale').innerHTML = 'Il prezzo del tuo biglietto è di € ' + costoBiglietto + '!';
